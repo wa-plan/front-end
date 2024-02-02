fd69fb4df3f5f415ba69400a5e9d97a4627e61f4
luenechoi <156325695+luenechoi@users.noreply.github.com>
2024-02-03 01:57:27 +0900

    전체 UI 테마 적용

---
+++ style.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+var theme = ThemeData(
+  appBarTheme: AppBarTheme(
+    color: Colors.white,
+    titleTextStyle: TextStyle(color: Colors.white, fontSize: 25, fontWeight: FontWeight.w500),
+    actionsIconTheme: IconThemeData(color: Colors.white),
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    selectedItemColor: Colors.white,
+  ),
+);
