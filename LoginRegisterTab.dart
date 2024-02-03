0e72b5a0b848793b31981b9019f68115d4482ebe
luenechoi <156325695+luenechoi@users.noreply.github.com>
2024-02-03 18:26:17 +0900

    로그인 및 회원가입 탭전환 수정

--- LoginRegisterTab.dart
+++ LoginRegisterTab.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 class TabBarScreen extends StatefulWidget {
   const TabBarScreen({Key? key}) : super(key: key);
 
@@ -20,20 +21,78 @@ class _TabBarScreenState extends State<TabBarScreen> with SingleTickerProviderSt
     tabController.dispose();
     super.dispose();
   }
+@override
+  void initState() {
+    tabController.addListener(() {
+      /// 프레임당 콜백
+    });
 
+    /// 탭바의 index가 변경되고 있는지 체크
+    /// true or false
+    tabController.indexIsChanging;
+
+    /// 탭바의 개수
+    tabController.length;
+
+    /// 변경 전의 index
+    tabController.previousIndex;
+
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("TabBarScreen"),
+        title: const Text("LoginMainPage"),
       ),
-      body: _tabBar(),
+  body: Column(
+        children: [
+          _tabBar(),
+          ElevatedButton(
+            onPressed: () {
+              /// 애니메이션 없이 바로 변경
+              tabController.index = 2;
+            },
+            child: const Text("이동"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              /// 애니메이션 없이 바로 변경
+              tabController.animateTo(5);
+            },
+            child: const Text("이동 animateTo"),
+          ),
+        ],
+      ),
     );
   }
 
+  //탭 색상 및 폰트
   Widget _tabBar() {
     return TabBar(
       controller: tabController,
+      labelColor: Colors.white,
+      unselectedLabelColor: Colors.grey,
+      labelStyle: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+      unselectedLabelStyle: const TextStyle(
+        fontSize: 16,
+      ),
+
+      /// indicator에서  UnderlineTabIndicator 미사용
+      indicatorWeight: 0,
+
+      indicatorSize: TabBarIndicatorSize.label,
+
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 20,
+      ),
+      
+      onTap: (index) {},
       tabs: const [
         Tab(text: "로그인"),
         Tab(text: "회원가입"),
