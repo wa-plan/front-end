be59c8b9e52a2acce66d3970d060b0962f7d8812
luenechoi <156325695+luenechoi@users.noreply.github.com>
2024-02-09 21:53:18 +0900

    회원가입

--- memberRegisterPage.dart
+++ memberRegisterPage.dart
@@ -71,27 +71,8 @@ _userProvider.email = _emailConroller.text;
         r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
     RegExp regExp = new RegExp(pattern);
 	
-    // 중복 이메일 검사
-    bool duplicate = false;
-    try {
-      var duplicate_email =
-          await _firestore.get().then((QuerySnapshot snapshot) {
-        snapshot.docs.forEach((doc) {
-          print(doc["email"]);
-          if (_emailConroller.text == doc["email"]) {
-            duplicate = true;
-          }
-        });
-      });
-    } catch (e) {
-      print(e);
-      duplicate = false;
-    }
-    if (duplicate) {
-      setState(() {
-        _errorMsg = "이미 이메일이 존재합니다.";
-      });
-    } else if (!regExp.hasMatch(_emailConroller.text)) {
+    /*  번호 검사
+     if (!regExp.hasMatch(_emailConroller.text)) {
       setState(() {
         _errorMsg = "올바르게 이메일을 입력하세요";
       });
@@ -102,4 +83,4 @@ _userProvider.email = _emailConroller.text;
       _userProvider.email = _emailConroller.text;
       _submit();
     }
-  }
+  } */
