9e83feb6708b042844c0fc98f49a2b604bd55916
chaemd <127921476+chaemd@users.noreply.github.com>
2024-05-19 01:27:27 +0900

    StatelessWidget으로 변경

---
+++ main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/settings/settings_main.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+import 'package:flutter_application_1/todayDomino/widgets/nav_bar.dart';
+
+import 'package:flutter_application_1/todayDomino/widgets/old_event_calendar.dart';
+
+void main() async {
+  await initializeDateFormatting();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'TDYdomino',
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Padding(
+            padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
+            child: Text(
+              '오늘의 도미노',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: MediaQuery.of(context).size.width * 0.06,
+                  fontWeight: FontWeight.w600),
+            ),
+          ),
+          backgroundColor: const Color(0xff262626),
+        ),
+        backgroundColor: const Color(0xff262626),
+        body: Column(children: [
+          const Expanded(
+            child: OldEventCalendar(),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SettingsMain(),
+                    ));
+              },
+              child: const Text('설정', style: TextStyle(color: Colors.white))),
+        ]),
+        bottomNavigationBar: const NavBar());
+  }
+}
