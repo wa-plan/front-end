31b6f5fb621727105402aaeb28e31919c5a869dd
chaemd <127921476+chaemd@users.noreply.github.com>
2024-06-26 23:41:06 +0900

    provider 적용

--- add_calendar.dart
+++ add_calendar.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/provider/date_provider.dart';
+import 'package:provider/provider.dart';
+//import 'package:flutter_application_1/todayDomino/widgets/event_calendar.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter_application_1/todayDomino/widgets/repeat_settings.dart';
 
 class AddCalendar extends StatefulWidget {
   const AddCalendar({Key? key}) : super(key: key);
-
   @override
   State<AddCalendar> createState() => AddCalendarState();
 }
 
 class AddCalendarState extends State<AddCalendar> {
+  late DateProvider _dateProvider;
+
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   late DateTime pickedDate = DateTime.now(); //도미노로 저장할 때, 해당 페이지로 넘길 날짜 변수
   RepeatSettingsState repeatSettings =
       RepeatSettingsState(); // RepeatSettingsState 인스턴스 생성
   final GlobalKey<RepeatSettingsState> repeatSettingsKey = GlobalKey();
-
-  DateTime getPickedDate() {
-    return pickedDate;
-  }
+  Map pickDates = {};
 
   @override
   void initState() {
     super.initState();
+    _dateProvider = Provider.of<DateProvider>(context, listen: false);
     pickedDate = _focusedDay; // 초기에는 현재 날짜로 설정
   }
 
@@ -45,15 +47,8 @@ class AddCalendarState extends State<AddCalendar> {
                 _selectedDay = selectedDay;
                 _focusedDay = focusedDay;
                 pickedDate = selectedDay;
-
-                //bool everyDay = repeatSettingsKey.currentState!.everyDay;
-                /*bool everyDay = repeatSettings.everyDay;
-                if (everyDay) {
-                  _selectedDay = selectedDay.add(const Duration(days: 1));
-                }*/
+                _dateProvider.pick();
               });
-              //bool repeatChoice = repeatSettingsKey.currentState!.repeatChoice();
-              //bool everyDay = repeatSettingsKey.currentState!.everyDay;
             }
           },
           onPageChanged: (focusedDay) {
@@ -98,3 +93,23 @@ class AddCalendarState extends State<AddCalendar> {
     );
   }
 }
+
+/*table calendar 이벤트 load 할 때 참고하기
+class Event {
+  final DateTime date;
+  final String content;
+  Event({required this.date, required this.content});
+}
+
+final _events = LinkedHashMap<DateTime, List<Event>>(
+  equals: isSameDay,
+  hashCode: getHashCode,
+)..addAll({
+    DateTime(2024, 6, 21): [
+      Event(date: DateTime(2024, 6, 21), content: "It's event1")
+    ],
+  });
+
+List<Event> _getEventsForDay(DateTime day) {
+  return _events[day] ?? [];
+}*/
